9b724a75804cb4592a13e43ae190f954e1e3e5e2
Vitaly Mikhailov <personal@aragas.org>
2020-04-08 14:54:59 +0300

    Mod rewritten

---
+++ MBFastDialogue/MountAndBlade/FastDialogueSubModule.cs
@@ -0,0 +1,40 @@
+﻿using HarmonyLib;
+
+using MBFastDialogue.CampaignSystem.CampaignBehaviors;
+using MBFastDialogue.StoryMode.GameModels;
+
+using System;
+
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace MBFastDialogue
+{
+	public class FastDialogueSubModule : MBSubModuleBase
+	{
+		public FastDialogueSubModule()
+		{
+			try
+			{
+				var harmony = new Harmony("io.dallen.bannerlord.fastdialogue");
+				harmony.PatchAll(typeof(FastDialogueSubModule).Assembly);
+			}
+			catch (Exception ex)
+			{
+				// TODO: Find a logger
+			}
+		}
+
+		protected override void OnGameStart(Game game, IGameStarter gameStarter)
+		{
+			if (game.GameType is Campaign campaign && gameStarter is CampaignGameStarter campaignGameStarter)
+			{
+				campaignGameStarter.AddBehavior(new FastDialogueCampaignBehaviorBase());
+
+				// Patching used instead.
+				//campaignGameStarter.AddModel(new FastDialogStoryModeEncounterGameMenuModel());
+			}
+		}
+	}
+}
